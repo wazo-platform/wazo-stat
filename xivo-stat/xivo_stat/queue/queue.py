347a7a5678f678a6fa46725024415ad87c031196
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-07-05 10:11:31 -0400

    [evol] add logic to add new entries in the call_on_queue table

--- xivo-stat/xivo_stat/queue/queue.py
+++ xivo-stat/xivo_stat/queue/queue.py
@@ -1,5 +1,12 @@
 # -*- coding: UTF-8 -*-
 
+from xivo_stat.dao import queue_log_dao
+from xivo_stat.dao import call_on_queue_dao
 
-def fill_full():
-    pass
+
+def fill_full_call(start, end):
+    full_calls = queue_log_dao.get_queue_full_call(start, end)
+    for call in full_calls:
+        call_on_queue_dao.add_full_call(call['callid'],
+                                        call['time'],
+                                        call['queue_name'])
--- xivo-stat/xivo_stat/queue/tests/test_queue.py
+++ xivo-stat/xivo_stat/queue/tests/test_queue.py
@@ -1,17 +1,30 @@
 # -*- coding: UTF-8 -*-
 
+import datetime
 import unittest
+from mock import Mock, patch
+from xivo_stat.queue import queue
+
+get_full_call = Mock()
+add_full_call = Mock()
 
 
-class Test(unittest.TestCase):
+class TestQueue(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self._queue_name = 'my_queue'
 
-    def tearDown(self):
-        pass
-
+    @patch('xivo_stat.dao.queue_log_dao.get_queue_full_call', get_full_call)
+    @patch('xivo_stat.dao.call_on_queue_dao.add_full_call', add_full_call)
     def test_fill_full(self):
-        expected_result = ''
+        d1 = datetime.datetime(2012, 01, 01, 00, 00, 00, 0000).strftime("%Y-%m-%d %H:%M:%S.%f")
+        d2 = datetime.datetime(2012, 01, 01, 23, 59, 59, 9999).strftime("%Y-%m-%d %H:%M:%S.%f")
+        callid = '1234567.890'
+        get_full_call.return_value = [{'queue_name': self._queue_name,
+                                       'event': 'full',
+                                       'time': d1,
+                                       'callid': callid}]
 
-        self.assertEqual(expected_result, '')
+        queue.fill_full_call(d1, d2)
+
+        add_full_call.assert_called_once_with(callid, d1, self._queue_name)
