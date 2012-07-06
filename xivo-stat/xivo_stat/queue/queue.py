6415d5ef30bfc4ce778c536098252c9ac7a7c3ce
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-07-06 11:03:31 -0400

    [evol] start fill stats

--- xivo-stat/xivo_stat/queue/queue.py
+++ xivo-stat/xivo_stat/queue/queue.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+import datetime
 
 from xivo_dao import queue_log_dao
 from xivo_dao import stat_call_on_queue_dao
@@ -10,4 +11,18 @@ def fill_full_call(start, end):
         stat_call_on_queue_dao.add_full_call(call['callid'],
                                              call['time'],
                                              call['queue_name'])
+
+
+def fill_stats():
+    most_recent = stat_call_on_queue_dao.get_most_recent_time()
+    now = datetime.datetime.now()
+    start = datetime.datetime(most_recent.year,
+                              most_recent.month,
+                              most_recent.day,
+                              most_recent.hour)
+    end = datetime.datetime(now.year,
+                            now.month,
+                            now.day,
+                            now.hour - 1)
+    fill_full_call(start, end)
 
--- xivo-stat/xivo_stat/queue/tests/test_queue.py
+++ xivo-stat/xivo_stat/queue/tests/test_queue.py
@@ -7,6 +7,8 @@ from xivo_stat.queue import queue
 
 get_full_call = Mock()
 add_full_call = Mock()
+get_most_recent_time = Mock()
+fill_full_call = Mock()
 
 
 class TestQueue(unittest.TestCase):
@@ -28,3 +30,20 @@ class TestQueue(unittest.TestCase):
         queue.fill_full_call(d1, d2)
 
         add_full_call.assert_called_once_with(callid, d1, self._queue_name)
+
+    @patch('xivo_dao.call_on_queue_dao.get_most_recent_time', get_most_recent_time)
+    @patch('xivo_stat.queue.queue.fill_full_call', fill_full_call)
+    def test_fill_stats(self):
+        now = datetime.datetime.now()
+        most_recent_time = now - datetime.timedelta(hours=23, minutes=55)
+        get_most_recent_time.return_value = most_recent_time
+        expected_start = datetime.datetime(most_recent_time.year,
+                                           most_recent_time.month,
+                                           most_recent_time.day,
+                                           most_recent_time.hour)
+        expected_stop = datetime.datetime(now.year, now.month, now.day, now.hour - 1)
+
+        queue.fill_stats()
+
+        fill_full_call.assert_called_once_with(expected_start, expected_stop)
+
