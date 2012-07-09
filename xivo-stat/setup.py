3fafe5f073627458d04be889f57c629ef16f0de9
Cedric Abunar <cabunar@avencall.com>
2012-07-09 13:10:34 -0400

    [evol]add Makefile and setup.py for packaging

---
+++ xivo-stat/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='xivo-stat',
+    version='0.1',
+    description='XiVO Stat Generation Script',
+    author='Avencall',
+    author_email='dev@avencall.com',
+    url='http://git.xivo.fr/',
+    license='GPLv3',
+    packages=['xivo_stat'],
+    scripts=['bin/xivo-stat'],
+)
