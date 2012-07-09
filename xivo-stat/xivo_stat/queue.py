56cf214c7421af9e3ec164eceddba67fc1e0d928
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-07-09 08:18:21 -0400

    [evol] add insert_periodic_stat to queue

--- xivo-stat/xivo_stat/queue.py
+++ xivo-stat/xivo_stat/queue.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 from xivo_dao import queue_log_dao
 from xivo_dao import stat_call_on_queue_dao
+from xivo_dao import stat_queue_periodic_dao
+import xivo_stat
 
 
 def fill_full_call(start, end):
@@ -16,4 +20,7 @@ def fill_calls(start, end):
 
 
 def insert_periodic_stat(start, end):
-    pass
+    period = datetime.timedelta(hours=1)
+    for t in xivo_stat.core.gen_time(start, end, period):
+        stats = stat_call_on_queue_dao.get_periodic_stats(t, t + period - datetime.timedelta(microseconds=1))
+        stat_queue_periodic_dao.insert_stats(stats, t)
--- xivo-stat/xivo_stat/tests/test_queue.py
+++ xivo-stat/xivo_stat/tests/test_queue.py
@@ -2,7 +2,7 @@
 import datetime
 import unittest
 
-from mock import Mock, patch
+from mock import Mock, patch, call
 
 from xivo_stat import queue
 
@@ -11,13 +11,18 @@ mock_add_full_call = Mock()
 mock_fill_full_call = Mock()
 mock_get_full_call = Mock()
 mock_get_most_recent_time = Mock()
+mock_get_periodic_stats = Mock()
 mock_insert_periodic_stat = Mock()
+mock_insert_stats = Mock()
+
 
 mocks = [mock_add_full_call,
          mock_fill_full_call,
          mock_get_full_call,
          mock_get_most_recent_time,
-         mock_insert_periodic_stat]
+         mock_get_periodic_stats,
+         mock_insert_periodic_stat,
+         mock_insert_stats]
 
 
 class TestQueue(unittest.TestCase):
@@ -52,3 +57,25 @@ class TestQueue(unittest.TestCase):
         queue.fill_calls(start, end)
 
         mock_fill_full_call.assert_called_once_with(start, end)
+
+    @patch('xivo_dao.stat_call_on_queue_dao.get_periodic_stats',
+           mock_get_periodic_stats)
+    @patch('xivo_dao.stat_queue_periodic_dao.insert_stats', mock_insert_stats)
+    def test_insert_periodic_stat(self):
+        s = datetime.datetime(2012, 01, 01)
+        e = datetime.datetime(2012, 01, 01, 4, 59, 59, 999999)
+
+        fake_stats = {'stats': 1234}
+
+        expected_periods = [datetime.datetime(2012, 01, 01, 0),
+                            datetime.datetime(2012, 01, 01, 1),
+                            datetime.datetime(2012, 01, 01, 2),
+                            datetime.datetime(2012, 01, 01, 3),
+                            datetime.datetime(2012, 01, 01, 4)]
+        expected_calls = [call(fake_stats, t) for t in expected_periods]
+
+        mock_get_periodic_stats.return_value = fake_stats
+
+        queue.insert_periodic_stat(s, e)
+
+        self.assertEqual(mock_insert_stats.call_args_list, expected_calls)
