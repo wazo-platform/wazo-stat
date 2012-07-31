ce1ffa76f0024d04dcaf775ec215352ec6d95313
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2012-07-31 17:07:41 -0400

    [refactor] Reduce the amount of queries when filling call on queue

--- xivo-stat/xivo_stat/queue.py
+++ xivo-stat/xivo_stat/queue.py
@@ -7,24 +7,20 @@ from xivo_dao import stat_call_on_queue_dao
 from xivo_dao import stat_queue_periodic_dao
 
 
-def fill_full_call(start, end):
-    full_calls = queue_log_dao.get_queue_full_call(start, end)
-    for call in full_calls:
-        stat_call_on_queue_dao.add_full_call(call['callid'],
-                                             call['time'],
-                                             call['queue_name'])
+def fill_full_call(started_calls):
+    full_calls = filter(lambda c: c.event == 'FULL', started_calls)
+    for c in full_calls:
+        stat_call_on_queue_dao.add_full_call(c.callid, c.time, c.queuename)
 
 
-def fill_joinempty_call(start, end):
-    joinempty_calls = queue_log_dao.get_queue_joinempty_call(start, end)
-    for call in joinempty_calls:
-        stat_call_on_queue_dao.add_joinempty_call(call['callid'],
-                                                  call['time'],
-                                                  call['queue_name'])
+def fill_joinempty_call(started_calls):
+    joinempty_calls = filter(lambda c: c.event == 'JOINEMPTY', started_calls)
+    for c in joinempty_calls:
+        stat_call_on_queue_dao.add_joinempty_call(c.callid, c.time, c.queuename)
 
 
-def fill_leaveempty_call(start, end):
-    leaveempty_calls = queue_log_dao.get_queue_leaveempty_call(start, end)
+def fill_leaveempty_call(started_calls):
+    leaveempty_calls = queue_log_dao.get_queue_leaveempty_call(started_calls)
     for call in leaveempty_calls:
         stat_call_on_queue_dao.add_leaveempty_call(call['callid'],
                                                    call['time'],
@@ -32,8 +28,8 @@ def fill_leaveempty_call(start, end):
                                                    call['waittime'])
 
 
-def fill_abandoned_call(start, end):
-    abandoned_calls = queue_log_dao.get_queue_abandoned_call(start, end)
+def fill_abandoned_call(started_calls):
+    abandoned_calls = queue_log_dao.get_queue_abandoned_call(started_calls)
     for call in abandoned_calls:
         stat_call_on_queue_dao.add_abandoned_call(call['callid'],
                                                   call['time'],
@@ -41,16 +37,14 @@ def fill_abandoned_call(start, end):
                                                   call['waittime'])
 
 
-def fill_closed_call(start, end):
-    closed_calls = queue_log_dao.get_queue_closed_call(start, end)
-    for call in closed_calls:
-        stat_call_on_queue_dao.add_closed_call(call['callid'],
-                                               call['time'],
-                                               call['queue_name'])
+def fill_closed_call(started_calls):
+    closed_calls = filter(lambda c: c.event == 'CLOSED', started_calls)
+    for c in closed_calls:
+        stat_call_on_queue_dao.add_closed_call(c.callid, c.time, c.queuename)
 
 
-def fill_answered_call(start, end):
-    answered_calls = queue_log_dao.get_queue_answered_call(start, end)
+def fill_answered_call(started_calls):
+    answered_calls = queue_log_dao.get_queue_answered_call(started_calls)
     for call in answered_calls:
         stat_call_on_queue_dao.add_answered_call(call['callid'],
                                                  call['time'],
@@ -58,8 +52,8 @@ def fill_answered_call(start, end):
                                                  call['waittime'])
 
 
-def fill_timeout_call(start, end):
-    timeout_calls = queue_log_dao.get_queue_timeout_call(start, end)
+def fill_timeout_call(started_calls):
+    timeout_calls = queue_log_dao.get_queue_timeout_call(started_calls)
     for call in timeout_calls:
         stat_call_on_queue_dao.add_timeout_call(call['callid'],
                                                 call['time'],
@@ -68,17 +62,20 @@ def fill_timeout_call(start, end):
 
 
 def fill_calls(start, end):
-    fill_abandoned_call(start, end)
-    fill_answered_call(start, end)
-    fill_closed_call(start, end)
-    fill_full_call(start, end)
-    fill_joinempty_call(start, end)
-    fill_leaveempty_call(start, end)
-    fill_timeout_call(start, end)
+    started_calls = queue_log_dao.get_started_calls(start, end)
+
+    fill_abandoned_call(started_calls)
+    fill_answered_call(started_calls)
+    fill_closed_call(started_calls)
+    fill_full_call(started_calls)
+    fill_joinempty_call(started_calls)
+    fill_leaveempty_call(started_calls)
+    fill_timeout_call(started_calls)
 
 
 def insert_periodic_stat(start, end):
     period = datetime.timedelta(hours=1)
     for t in xivo_stat.core.gen_time(start, end, period):
-        stats = stat_call_on_queue_dao.get_periodic_stats(t, t + period - datetime.timedelta(microseconds=1))
-        stat_queue_periodic_dao.insert_stats(stats, t,)
+        stats = stat_call_on_queue_dao.get_periodic_stats(
+            t, t + period - datetime.timedelta(microseconds=1))
+        stat_queue_periodic_dao.insert_stats(stats, t)
