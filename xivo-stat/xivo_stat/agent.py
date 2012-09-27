25ee390e1511438e6e33880336ef59d1d3b1a248
Cedric Abunar <cabunar@avencall.com>
2012-09-27 14:45:45 -0400

    [evo #3673]add pause time to agent

--- xivo-stat/xivo_stat/agent.py
+++ xivo-stat/xivo_stat/agent.py
@@ -10,11 +10,35 @@ from xivo_dao import stat_dao
 ONE_HOUR = timedelta(hours=1)
 
 
+def _merge_update_agent_statistics(*args):
+    result = {}
+
+    for stats in args:
+        for time_interval, stat in stats.iteritems():
+            if time_interval not in result:
+                result[time_interval] = {}
+            for agent, time_map in stat.iteritems():
+                if agent not in result[time_interval]:
+                    result[time_interval][agent] = time_map
+                else:
+                    for time_label, time in time_map.iteritems():
+                        current_val = result[time_interval][agent].get(time_label, timedelta(seconds=0))
+                        result[time_interval][agent][time_label] = time + current_val
+
+    return result
+
 def insert_periodic_stat(start, end):
     print 'Inserting agent periodic stat'
+    time_computer = AgentTimeComputer(start, end, ONE_HOUR)
+
     login_intervals = stat_dao.get_login_intervals_in_range(start, end)
-    login_computer = AgentLoginTimeComputer(start, end, ONE_HOUR)
-    periodic_stats = login_computer.compute_login_time_in_period(login_intervals)
+    pause_intervals = stat_dao.get_pause_intervals_in_range(start, end)
+
+    periodic_stats_login = time_computer.compute_login_time_in_period(login_intervals)
+    periodic_stats_pause = time_computer.compute_pause_time_in_period(pause_intervals)
+
+    periodic_stats = _merge_update_agent_statistics(periodic_stats_login, periodic_stats_pause)
+
     for period, stats in periodic_stats.iteritems():
         stat_agent_periodic_dao.insert_stats(stats, period)
 
@@ -24,7 +48,7 @@ def remove_after_start(date):
     stat_agent_periodic_dao.remove_after(date)
 
 
-class AgentLoginTimeComputer(object):
+class AgentTimeComputer(object):
 
     def __init__(self, start, end, interval_size):
         self.start = start
@@ -32,31 +56,36 @@ class AgentLoginTimeComputer(object):
         self.interval_size = interval_size
         self.possible_intervals = list(time_utils.gen_time(start, end, interval_size))
 
-    def compute_login_time_in_period(self, login_sessions_by_agent):
+    def compute_login_time_in_period(self, sessions_by_agent):
+        return self._compute_time_in_period('login_time', sessions_by_agent)
+
+    def compute_pause_time_in_period(self, sessions_by_agent):
+        return self._compute_time_in_period('pause_time', sessions_by_agent)
+
+    def _compute_time_in_period(self, type, sessions_by_agent):
         results = {}
 
-        for agent, login_sessions in login_sessions_by_agent.iteritems():
-            for login_start, login_end in login_sessions:
+        for agent, sessions in sessions_by_agent.iteritems():
+            for time_start, time_end in sessions:
                 touched_periods = time_utils.get_period_start_for_time_range(
-                    self.possible_intervals, login_start, login_end)
+                    self.possible_intervals, time_start, time_end)
 
                 for period_start in touched_periods:
                     if period_start not in results:
                         results[period_start] = {}
                     period_end = period_start + self.interval_size
-                    end_in_interval = min(login_end, period_end)
-                    start_in_interval = max(login_start, period_start)
+                    end_in_interval = min(time_end, period_end)
+                    start_in_interval = max(time_start, period_start)
                     delta = end_in_interval - start_in_interval
                     self._add_time_to_agent_in_period(
-                        results[period_start], agent, delta)
+                        results[period_start], agent, type, delta)
 
         return results
 
-    def _add_time_to_agent_in_period(self, period, agent, duration):
-        if duration == timedelta(0):
-            return
-
+    def _add_time_to_agent_in_period(self, period, agent, time_type, duration):
         if agent not in period:
-            period[agent] = duration
+            period[agent] = {}
+        if time_type not in period[agent]:
+            period[agent][time_type] = duration
         else:
-            period[agent] += duration
+            period[agent][time_type] += duration
--- xivo-stat/xivo_stat/tests/test_agent.py
+++ xivo-stat/xivo_stat/tests/test_agent.py
@@ -8,6 +8,8 @@ from datetime import datetime as dt
 from datetime import timedelta
 
 from xivo_stat import agent
+from xivo_dao.stat_dao import _merge_agent_statistics
+from xivo_stat.agent import _merge_update_agent_statistics
 
 
 ONE_HOUR = timedelta(hours=1)
@@ -16,11 +18,13 @@ ONE_HOUR = timedelta(hours=1)
 mock_get_login_intervals_in_range = Mock()
 mock_insert_stats = Mock()
 mock_stat_agent_periodic_remove_after = Mock()
+mock_get_pause_intervals_in_range = Mock()
 
 
 mocks = [mock_get_login_intervals_in_range,
          mock_insert_stats,
          mock_stat_agent_periodic_remove_after,
+         mock_get_pause_intervals_in_range
          ]
 
 
@@ -33,6 +37,8 @@ class TestAgent(unittest.TestCase):
            mock_insert_stats)
     @patch('xivo_dao.stat_dao.get_login_intervals_in_range',
            mock_get_login_intervals_in_range)
+    @patch('xivo_dao.stat_dao.get_pause_intervals_in_range',
+           mock_get_pause_intervals_in_range)
     def test_insert_periodic_stat(self):
         agent_id_1 = 12
         agent_id_2 = 13
@@ -47,26 +53,34 @@ class TestAgent(unittest.TestCase):
             }
         output_stats = {
             dt(2012, 01, 01, 01, 00, 00): {
-                agent_id_1: timedelta(minutes=50),
-                agent_id_2: ONE_HOUR,
+                agent_id_1: {'login_time' : timedelta(minutes=50),
+                             'pause_time': timedelta(minutes=13)},
+                agent_id_2: {'login_time' : ONE_HOUR,
+                             'pause_time': timedelta(minutes=13)},
                 },
             dt(2012, 01, 01, 02, 00, 00): {
-                agent_id_1: timedelta(minutes=20),
-                agent_id_2: ONE_HOUR,
+                agent_id_1: {'login_time' : timedelta(minutes=20),
+                             'pause_time': timedelta(minutes=33)},
+                agent_id_2: {'login_time' : ONE_HOUR,
+                             'pause_time': timedelta(minutes=13)},
                 },
             dt(2012, 01, 01, 03, 00, 00): {
-                agent_id_2: ONE_HOUR,
+                agent_id_2: {'login_time' : ONE_HOUR,
+                             'pause_time': ONE_HOUR},
                 },
             dt(2012, 01, 01, 04, 00, 00): {
-                agent_id_2: ONE_HOUR,
+                agent_id_2: {'login_time' : ONE_HOUR,
+                             'pause_time': ONE_HOUR},
                 }
             }
         start = dt(2012, 01, 01, 01, 00, 00)
         end = dt(2012, 01, 01, 04, 00, 00)
 
         mock_get_login_intervals_in_range.return_value = input_stats
-        login_time_computer = Mock(agent.AgentLoginTimeComputer)
-        login_time_computer.compute_login_time_in_period.return_value = output_stats
+        mock_get_pause_intervals_in_range.return_value = input_stats
+        time_computer = Mock(agent.AgentTimeComputer)
+        time_computer.compute_login_time_in_period.return_value = output_stats
+        time_computer.compute_pause_time_in_period.return_value = output_stats
 
         agent.insert_periodic_stat(start, end)
 
@@ -77,12 +91,92 @@ class TestAgent(unittest.TestCase):
 
 class TestAgentLoginTimeComputer(unittest.TestCase):
 
-    def test_compute_login_time_in_period(self):
+    def test__add_time_to_agent_in_period(self):
+        start = dt(2012, 1, 1)
+        end = dt(2012, 1, 1, 23, 59, 59, 999999)
+        interval_size = ONE_HOUR
+
+        period = { dt(2012, 01, 01, 01, 00, 00): {} }
+        agent_id = 1
+        time_type = 'pause_time'
+        duration = timedelta(minutes=10)
+
+        computer = agent.AgentTimeComputer(
+            start,
+            end,
+            interval_size,
+            )
+
+        expected_result = {dt(2012, 01, 01, 01, 00, 00): {1: {'pause_time': duration}} }
+
+        computer._add_time_to_agent_in_period(period[dt(2012, 1, 1, 1, 0, 0)], agent_id, time_type, duration)
+
+        self.assertEqual(period, expected_result)
+
+    def test_merge_update_agent_statistics(self):
+        agent_id_1, agent_id_2 = 12, 23
+        stat1 = {dt(2012, 01, 01, 01, 00, 00): {agent_id_1: {'login_time' : timedelta(minutes=50)},
+                                                agent_id_2: {'login_time' : ONE_HOUR}},
+            dt(2012, 01, 01, 02, 00, 00): {agent_id_1: {'login_time' : timedelta(minutes=20)},
+                                           agent_id_2: {'login_time' : ONE_HOUR},
+                },
+            dt(2012, 01, 01, 03, 00, 00): {agent_id_2: {'login_time' : ONE_HOUR},
+                },
+            dt(2012, 01, 01, 04, 00, 00): {agent_id_2: {'login_time' : ONE_HOUR},
+                }
+            }
+
+        stat2 = {dt(2012, 01, 01, 01, 00, 00): {
+                 agent_id_1: {'pause_time': timedelta(minutes=13)},
+                 agent_id_2: {'pause_time': timedelta(minutes=13)},
+                },
+            dt(2012, 01, 01, 02, 00, 00): {
+                agent_id_1: {'pause_time': timedelta(minutes=33)},
+                agent_id_2: {'pause_time': timedelta(minutes=13)},
+                },
+            dt(2012, 01, 01, 03, 00, 00): {
+                agent_id_2: {'pause_time': ONE_HOUR},
+                },
+            dt(2012, 01, 01, 04, 00, 00): {
+                agent_id_2: {'pause_time': ONE_HOUR},
+                }
+            }
+
+        expected = {dt(2012, 01, 01, 01, 00, 00): {
+                 agent_id_1: {'login_time' : timedelta(minutes=50),
+                             'pause_time': timedelta(minutes=13)},
+                 agent_id_2: {'login_time' : ONE_HOUR,
+                             'pause_time': timedelta(minutes=13)},
+                },
+            dt(2012, 01, 01, 02, 00, 00): {
+                agent_id_1: {'login_time' : timedelta(minutes=20),
+                             'pause_time': timedelta(minutes=33)},
+                agent_id_2: {'login_time' : ONE_HOUR,
+                             'pause_time': timedelta(minutes=13)},
+                },
+            dt(2012, 01, 01, 03, 00, 00): {
+                agent_id_2: {'login_time' : ONE_HOUR,
+                             'pause_time': ONE_HOUR},
+                },
+            dt(2012, 01, 01, 04, 00, 00): {
+                agent_id_2: {'login_time' : ONE_HOUR,
+                             'pause_time': ONE_HOUR},
+                }
+            }
+
+        result = agent._merge_update_agent_statistics(stat1, stat2)
+
+        self.assertEqual(result, expected)
+
+    def test_compute_time_in_period(self):
         agent_id_1 = 1
         agent_id_2 = 2
 
-        start = dt(2012, 1, 1)
-        end = dt(2012, 1, 1, 23, 59, 59, 999999)
+        computer = agent.AgentTimeComputer(
+            dt(2012, 1, 1),
+            dt(2012, 1, 1, 23, 59, 59, 999999),
+            ONE_HOUR,
+        )
 
         logins = {
             agent_id_1: [
@@ -94,34 +188,27 @@ class TestAgentLoginTimeComputer(unittest.TestCase):
                 ]
             }
 
-        interval_size = ONE_HOUR
-
         expected = {
             dt(2012, 01, 01, 01, 00, 00): {
-                agent_id_1: timedelta(minutes=50),
-                agent_id_2: ONE_HOUR,
+                agent_id_1: {'login_time' : timedelta(minutes=50)},
+                agent_id_2: {'login_time' : ONE_HOUR}
                 },
             dt(2012, 01, 01, 02, 00, 00): {
-                agent_id_1: timedelta(minutes=20),
-                agent_id_2: ONE_HOUR,
+                agent_id_1: {'login_time' : timedelta(minutes=20)},
+                agent_id_2: {'login_time' : ONE_HOUR}
                 },
             dt(2012, 01, 01, 03, 00, 00): {
-                agent_id_2: ONE_HOUR,
+                agent_id_2: {'login_time' : ONE_HOUR}
                 },
             dt(2012, 01, 01, 04, 00, 00): {
-                agent_id_2: ONE_HOUR,
+                agent_id_2: {'login_time' : ONE_HOUR}
                 }
             }
 
-        computer = agent.AgentLoginTimeComputer(
-            start,
-            end,
-            interval_size,
-            )
-
-        result = computer.compute_login_time_in_period(logins)
+        result = computer._compute_time_in_period('login_time', logins)
 
         self.assertEqual(result, expected)
+
 
     @patch('xivo_dao.stat_agent_periodic_dao.remove_after', mock_stat_agent_periodic_remove_after)
     def test_remove_after_start(self):
